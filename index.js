8c0c753febdab9157e698db6b01f60c4240247a6
hilminaufal86 <41366525+hilminaufal86@users.noreply.github.com>
2020-12-13 16:59:25 +0700

    init telegram bot

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Telegraf } = require('telegraf')
-BOT_TOKEN = '1466037800:AAEBslgYakxBUEr3DRsTB3oph0UHPawPPf0'
+// const { Telegraf } = require('telegraf')
+// BOT_TOKEN = '1466037800:AAEBslgYakxBUEr3DRsTB3oph0UHPawPPf0'
+const { Composer } = require('micro-bot')
+const bot = new Composer
 
-const bot = new Telegraf(BOT_TOKEN)
+// const bot = new Telegraf(BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => 
 ctx.reply(
@@ -9,4 +11,8 @@ ctx.reply(
     '\\help: help'))
 bot.on('sticker', (ctx) => ctx.reply(''))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+// bot.launch()
+module.exports = bot
+
+// server-id  : lit-beyond-85890
+// server-add : https://lit-beyond-85890.herokuapp.com/
