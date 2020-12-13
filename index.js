9c0ea14d4853d3e52610ea4afa9b3a53ca31ab44
hilminaufal86 <41366525+hilminaufal86@users.noreply.github.com>
2020-12-13 19:09:58 +0700

    add track command

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 // const { Telegraf } = require('telegraf')
-// BOT_TOKEN = '1466037800:AAEBslgYakxBUEr3DRsTB3oph0UHPawPPf0'
 const { Composer } = require('micro-bot')
+const axios = require('axios');
+
 const bot = new Composer
 
-// const bot = new Telegraf(BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => 
-ctx.reply(
-    '\\start: start\n'+
-    '\\help: help'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-// bot.launch()
-module.exports = bot
+bot.help((ctx) => ctx.reply(
+    '/help: show list of available command\n'+
+    '/track: track your package delivery'
+    ))
 
-// server-id  : lit-beyond-85890
-// server-add : https://lit-beyond-85890.herokuapp.com/
+bot.command('track', (ctx) => {
+    let messageArray = ctx.message.text.split();
+    if (messageArray.length == 2) {
+        if (messageArray[1]=="list") {
+            ctx.reply("kurir list : jne, pos, jnt, sicepat, tiki, anteraja, wahana, ninja\n")
+        }
+        if (messageArray[1]=="help") {
+            ctx.reply("tracking  : /track [kurir] [no. resi]\n" + 
+                      "list kurir: /track list");
+        }
+    }
+    if (messageArray.length != 3) {
+        ctx.reply("for tracking information: /track help");
+        return 0;
+    }
+    
+    let kurir = messageArray[1].toLowerCase();
+    let resi = messageArray[2].toUpperCase();
+    let url = "https://api.binderbyte.com/v1/track";
+
+    axios.get(url, {
+        params: {
+            api_key : process.env.CEKRESI_API_KEY,
+            courier : kurir,
+            awb : resi
+        }
+    })
+    .then((result) => {
+        if (result.status == 200) {
+            let info = 'No. Resi : ' + result.data.summary.awb +'\n' +
+                       'Kurir    : ' + result.data.summary.corier + '\n' +
+                       'Layanan  : ' + result.data.summary.service + '\n' +
+                       'Asal     : ' + result.data.detail.origin + '\n' +
+                       'Tujuan   : ' + result.data.detail.destination + '\n' +
+                       'Pengirim : ' + result.data.detail.shipper + '\n' +
+                       'Penerima : ' + result.data.detail.receiver + '\n' +
+                       'Status   : ' + result.data.summary.status;
+            
+            ctx.reply(info);
+        }
+        else if (result.status == 400) {
+            ctx.reply("No. resi tidak ditemukan");
+        }
+        else {
+            ctx.reply("Error found, please try again later");
+        }
+    })
+
+})
+
+module.exports = bot
